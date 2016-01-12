a389cc5a3fb35989dc27bf0bf630627500fac2d9
Kasia Janowska <kasia.janowska@uj.edu.pl>
2016-01-12 19:56:39 +0100

    zliczanie zgłoszeń i początek testów

---
+++ spec/features/issues_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+feature "zarządzanie numerami" do
+  scenario "zarządzanie numerami bez uprawnień" do
+    visit '/issues'
+
+    expect(page).to have_content 'Log in'
+  end
+  
+  context "po zalogowaniu" do
+    include_context "admin login"
+    
+    scenario "dostępność numerów w menu" do
+      visit '/people'
+      
+      expect(page).to have_link("Numery rocznika")
+    end
+    
+    scenario "wyświetlanie pustej listy numerów" do
+      visit '/issues'
+      
+      expect(page).to have_css("h3", text: "Numery rocznika")
+    end
+    
+    scenario "link do nowego numeru" do
+      visit '/issues'
+      click_link 'Nowy numer'
+
+      expect(page).to have_css("#new_issue input[value='Utwórz']")
+    end
+    
+    scenario "tworzenie nowego numeru" do
+      visit '/issues/new'
+
+      within("#new_issue") do
+        fill_in "Numer", with: 2
+        fill_in "Rok", with: 2017
+      end
+      click_button 'Utwórz'
+
+      expect(page).not_to have_css(".has-error")
+      expect(page).to have_content(2)
+      expect(page).to have_content(2017)
+    end
+    
+    context "z jednym numerem w bazie danych" do
+    before do
+      Issue.create!(volume: 3, year: 2020)
+    end
+
+    scenario "wyświetlenie szczegółów numeru" do
+      visit "/issues"
+      click_link("3")
+
+      expect(page).to have_content("Numer 3/2020")
+    end
+    
+    scenario "dodawanie zgłoszenia z istniejącym numerem" do
+      visit '/submissions/new'
+
+      within("#new_submission") do
+        select "nadesłany", from: "Status"
+        select "polski", from: "Język"
+        select "3/2020", from: "Nr wydania"
+        fill_in "Otrzymano", with: "12-01-2016"
+        fill_in "Tytuł", with: "próbny tytuł"
+      end
+      click_button 'Utwórz'
+
+      expect(page).not_to have_css(".has-error")
+      expect(page).to have_content("3/2020")
+    end
+    
+      scenario "wyświetlenie numeru z dodanym zgłoszeniem" do
+      end
+    end
+  end
+  
+  
+end
