b40b61118cb511fe3c9ce8ec1a9f7a3f407c6a40
Maja Chłodnicka <maja.chlodnicka@uj.edu.pl>
2016-01-16 17:48:27 +0100

    Poprawki

--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :person
-  belongs_to :article_revision
-
-  validates :person_id, presence: true
-  validates :article_revision_id, presence: true
-end
--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :content, :person_id, :article_revision_id, :created_at, :updated_at
