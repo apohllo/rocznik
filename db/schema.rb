071627f76d73d2053dfd5ce4b56d23b3b3d2bb97
Adrianna Dabkowska <adrianna.dabkowska@host.com>
2016-01-20 12:11:39 +0100

    nie widzi pola status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110094020) do
+ActiveRecord::Schema.define(version: 20160114163740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20160110094020) do
     t.datetime "updated_at",              null: false
     t.text     "roles",      default: [], null: false, array: true
     t.string   "sex"
+    t.string   "photo"
   end
 
   add_index "people", ["email"], name: "index_people_on_email", using: :btree
