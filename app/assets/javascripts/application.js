a9c8cee483193db5296215edb9e6f2a47c766bcc
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2016-01-31 21:07:14 +0100

    Poprawne wyświetlanie guzika

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,25 +20,25 @@ $(document).ready(function() {
     // Configure/customize these variables.
     var showChar = 100; // How many characters are shown by default
     var ellipsestext = "...";
-    var moretext = "Zwiń";
-    var lesstext = "Czytaj dalej :>";
-    
+    var moretext = "<div class='btn btn-default btn-sm'>Czytaj dalej &raquo;</div>";
+    var lesstext = "<div class='btn btn-default btn-sm'>Zwiń</div>";
+
 
     $('.more').each(function() {
         var content = $(this).html();
- 
+
         if(content.length > showChar) {
- 
+
             var c = content.substr(0, showChar);
             var h = content.substr(showChar, content.length - showChar);
- 
+
             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
+
             $(this).html(html);
         }
- 
+
     });
- 
+
     $(".morelink").click(function(){
         if($(this).hasClass("less")) {
             $(this).removeClass("less");
