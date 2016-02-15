1f72a6fe7415243a2bf88c899e23e07a78a8ded3
Dmitrij Beskrestnov <rotoson@rambler.ru>
2016-02-15 13:38:19 +0100

    Poprawione testy!

--- spec/features/public_articles_spec.rb
+++ spec/features/public_articles_spec.rb
@@ -2,49 +2,31 @@ require 'rails_helper'
 
 
 feature "Sprawdzanie opublikowanych artykulów" do
-   include_context "admin login"
    before do 
      
-     person1 = Person.create(name: 'Adam', surname: 'Kapusta', email: 'a@k.com', sex: 'mężczyzna', roles: ['autor'])
-     person_id = Person.find_by_name("Adam")
-     issue_1 = Issue.create(year: 2001, volume: 1, published: true)
-     submission = Submission.create(polish_title: 'Wiemy wszystko', english_title: 'We know everything',
-                                        english_abstract: 'Tak po prostu', english_keywords: 'knowledge', person_id:person_id, 
-                                        issue: issue_1, language: 'polski', received: '28-01-2016',
+     person1 = Person.create!(name: 'Adam', surname: 'Kapusta', email: 'a@k.com', sex: 'mężczyzna', roles: ['autor'], discipline:['filozofia'])
+     issue_1 = Issue.create!(year: 2001, volume: 1, published: true)
+     submission1 = Submission.create!(polish_title: 'Wiemy wszystko', english_title: 'We know everything',
+                                        english_abstract: 'Tak po prostu', english_keywords: 'knowledge', 
+                                        issue: issue_1, person:person1, language: 'polski', received: '28-01-2016',
                                         status: 'przyjęty')
-     Article.create(submission: submission, issue: issue_1, status: 'opublikowany', DOI: 40000,
+     time1 = Time.now.to_i 
+     Authorship.create!(person:person1, submission: submission1)
+     Article.create!(submission: submission1, issue: issue_1, status: 'opublikowany', DOI: 40000,
                                         external_link:'http://bulka', pages:'300')
+    end
+
+   scenario "Tresc artykulu" do
      visit '/public_issues'
      click_on '1/2001'
      click_on 'Wiemy wszystko'
 
-   end
-
-   scenario "Tytul strony" do
      expect(page).to have_content("Wiemy wszystko") 
-   end
-
-   scenario "Autor" do
-     expect(page).to have_content("Adam Kapusta")  
-   end
-
-   scenario "Streszczenie" do
+     expect(page).to have_content("A. Kapusta")  
      expect(page).to have_content("Tak po prostu")  
-   end
-
-   scenario "Slowa kluczowe" do
      expect(page).to have_content("knowledge")  
-   end
-
-   scenario "Link do ściągnięcia" do
      expect(page).to have_content("http://bulka")  
-   end
-
-   scenario "Strony" do
      expect(page).to have_content("300")  
-   end
-
-   scenario "Numer" do
      expect(page).to have_content("1/2001")  
    end
 
--- spec/features/public_issues_spec.rb
+++ spec/features/public_issues_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 
 feature "Sprawdzanie opublikowanych numerów" do
    before do 
-     editor = Person.create(name: 'Adam', surname: 'Kapusta', email: 'a.kapusta@gmail.com', sex: 'mężczyzna', roles: ['redaktor'])
-     issue_1 = Issue.create(year: 2001, volume: 1, published: true)
-     submission = Submission.create(polish_title: 'Wiemy wszystko', english_title: 'We know everything',
+     editor = Person.create!(name: 'Adam', surname: 'Kapusta', email: 'a@k.com', sex: 'mężczyzna', roles: ['autor'], discipline:['filozofia'])
+     issue_1 = Issue.create!(year: 2001, volume: 1, published: true)
+     submission = Submission.create!(polish_title: 'Wiemy wszystko', english_title: 'We know everything',
                                         english_abstract: 'Tak po prostu', english_keywords: 'knowledge',
                                         person: editor, issue: issue_1, language: 'polski', received: '28-01-2016',
                                         status: 'przyjęty')
-     submission2 = Submission.create(polish_title: 'Jerzozwież', english_title: 'Porcupine',
+     submission2 = Submission.create!(polish_title: 'Jerzozwież', english_title: 'Porcupine',
                                         english_abstract: 'O jerzozwieżach', english_keywords: 'zwierzęta',
                                         person: editor, issue: issue_1, language: 'polski', received: '28-01-2016',
                                         status: 'przyjęty')
-     Article.create(submission: submission, issue: issue_1, status: 'po recenzji', DOI: 40000)
-     Article.create(submission: submission2, issue: issue_1, status: 'opublikowany', DOI: 42000)    
+     Article.create!(submission: submission, issue: issue_1, status: 'po recenzji', DOI: 40000)
+     Article.create!(submission: submission2, issue: issue_1, status: 'opublikowany', DOI: 42000)    
    end
 
    scenario "Linki do numerów" do
      visit '/public_issues'
     
      click_on '1/2001'
-     expect(current_path).to eq("/public_issues/1-2001")   
+    expect(page).to have_content("Wiemy wszystko")
    end
 
    scenario "Linki do artykułów" do
@@ -29,10 +29,7 @@ feature "Sprawdzanie opublikowanych numerów" do
 
      click_on '1/2001'
      click_on 'Wiemy wszystko'
-     
-     submission_id = Submission.find_by_polish_title("Wiemy wszystko").id
-     article_id = Article.find_by_submission_id(submission_id).id
-     expect(current_path).to eq("/public_articles/#{article_id}-wiemy-wszystko")
+     expect(page).to have_content("Tak po prostu")
    end
 end
  
