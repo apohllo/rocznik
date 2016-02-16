156b37333ee5533cd6fb4c4af02ddab21d8a915c
Łukasz Krawczyk <lukikrk@gmail.com>
2016-02-16 15:06:48 +0100

    Podejście pierwsze

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,6 +1,12 @@
 class ReviewMailer < ApplicationMailer
+
   def ask(review)
     @review = review
     mail(from: @review.article_revision.submission.person.email, to: @review.person.email, subject: 'Prośba o recenzję')
+  end
+  
+  def response(review)
+    @review = review
+    mail(from: @rewiev.article_revision.submission.person.email, to:@review.person.email, subject: 'Informacja o zmianie statusu')
   end
 end
