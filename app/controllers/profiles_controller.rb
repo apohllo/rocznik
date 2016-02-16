bb3ccb632cfb52f49930dd9d4df10d6809d026c8
mzet94 <zmarta2511@gmail.com>
2016-02-16 10:39:30 +0000

    zmiany w profiles_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,4 @@
-class ProfileController < PeopleController
+class ProfilesController < ApplicationController
   before_action :user_required
   
   def show
@@ -29,9 +29,15 @@ class ProfileController < PeopleController
   def update_password
     @user = current_user
     @person = Person.find_by_email(@user.email)
-    if @person.update_attributes(user_params)
-      sign_in @user, :bypass => true
-      redirect_to @person
+    if @user.valid_password?(params[:current_password], true)
+      @user.password = params[:password]
+      @user.password_confirmation = params[:password_confirmation]
+      if @user.update_attributes(user_params)
+        sign_in @user, :bypass => true
+        redirect_to @person
+      else
+        render :edit_password
+      end
     else
       render :edit_password
     end
@@ -40,5 +46,9 @@ class ProfileController < PeopleController
   private
   def user_params
     params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
+  
+  def person_params
+    params.require(:person).permit(:name,:surname,:degree,:email,:sex,:photo,:competence,roles: [], discipline: [])
   end
 end
