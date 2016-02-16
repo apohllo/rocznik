3e881bda7a125dda6918e2e6f3130e42cdcea33f
Dmitrij Beskrestnov <rotoson@rambler.ru>
2016-02-16 14:29:25 +0100

    Rozwiązany konflikt

--- spec/features/public_issues_spec.rb
+++ spec/features/public_issues_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 
 feature "Sprawdzanie opublikowanych numerów" do
-<<<<<<< HEAD
    before do 
      person1 = Person.create!(name: 'Adam', surname: 'Kapusta', email: 'a@k.com', sex: 'mężczyzna', roles: ['autor'], discipline:['filozofia'])
      issue_1 = Issue.create!(year: 2001, volume: 1, published: true)
@@ -15,23 +14,6 @@ feature "Sprawdzanie opublikowanych numerów" do
      Article.create!(submission: submission1, issue: issue_1, status: 'opublikowany', DOI: 40000,
                                         external_link:'http://bulka', pages:'300')
    end
-=======
-  before do
-    editor = Person.create!(name: 'Adam', surname: 'Kapusta', email: 'a@k.com', sex: 'mężczyzna', roles: ['autor'],
-                            discipline:['filozofia'])
-    issue_1 = Issue.create!(year: 2001, volume: 1, published: true)
-    submission = Submission.create!(polish_title: 'Wiemy wszystko', english_title: 'We know everything',
-                                    english_abstract: 'Tak po prostu', english_keywords: 'knowledge',
-                                    person: editor, issue: issue_1, language: 'polski', received: '28-01-2016',
-                                    status: 'przyjęty')
-    submission2 = Submission.create!(polish_title: 'Jerzozwież', english_title: 'Porcupine',
-                                     english_abstract: 'O jerzozwieżach', english_keywords: 'zwierzęta',
-                                     person: editor, issue: issue_1, language: 'polski', received: '28-01-2016',
-                                     status: 'przyjęty')
-    Article.create!(submission: submission, issue: issue_1, status: 'po recenzji', DOI: 40000)
-    Article.create!(submission: submission2, issue: issue_1, status: 'opublikowany', DOI: 42000)
-  end
->>>>>>> 70b2669e465fefee495e87c7221ab9b0bcecd1b2
 
   scenario "Linki do numerów" do
     visit '/public_issues'
