098bb66d3d33e434b4c03869206ca3a78db116e9
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2016-02-16 02:21:11 +0100

    Fix rubocop + icons

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -68,13 +68,13 @@ class ReviewsController < ApplicationController
     review.destroy
     redirect_to review.submission
   end
-  
- def ask
+
+  def ask
     review = Review.find(params[:id])
     ReviewMailer.ask(review).deliver_now
     redirect_to review.submission
- end
-  
+  end
+
   private
   def review_params
     params.require(:review).permit(:person_id,:status,:asked,:deadline,:remarks,:content,:article_revision_id)
