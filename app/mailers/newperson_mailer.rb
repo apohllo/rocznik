f2433e77fab39bab76b7b47813db098420d4da11
roksana1 <roksanawantulok@gmail.com>
2016-02-18 15:20:21 +0000

    152 Dodanie nowego użytkownika w trakcie zgłoszenia

--- app/mailers/newperson_mailer.rb
+++ app/mailers/newperson_mailer.rb
@@ -1,6 +1,7 @@
 class NewuserMailer < ApplicationMailer
   def add(person)
     @person = person
-    mail(from: @.article_revision.submission.person.email, to: @submission.person.email, subject: 'Informacja o nowym zgłoszeniu oraz rejesracji w systemie.')
+    mail(from: @.article_revision.submission.person.email,
+ to: @submission.person.email, subject: 'Informacja o nowym zgłoszeniu oraz rejesracji w systemie.')
   end
  end
