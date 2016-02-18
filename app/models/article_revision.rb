f23320dce77f0af112945b9ed3043950eb093537
gadi-promienna <gadi@gadi.org.pl>
2016-02-18 15:35:48 +0100

    troszke zmian do zadania 239

--- app/models/article_revision.rb
+++ app/models/article_revision.rb
@@ -7,6 +7,7 @@ class ArticleRevision < ActiveRecord::Base
   validates :pages, presence: true, numericality: true
   validates :pictures, presence: true, numericality: true
   validates :version, presence: true, numericality: true
+  validates :received, presence: true 
 
   def title
     "#{self.submission.title}, v. #{self.version}"
