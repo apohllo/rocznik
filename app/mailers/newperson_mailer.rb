d179a9fc3a66587bd0d3916979a811d7e293adb8
roksana1 <roksanawantulok@gmail.com>
2016-02-19 09:49:31 +0000

    152 dodanie nowego użytkownika po zgloszeniu

--- app/mailers/newperson_mailer.rb
+++ app/mailers/newperson_mailer.rb
@@ -1,7 +1,7 @@
 class NewuserMailer < ApplicationMailer
   def add(person)
     @person = person
-    mail(from: @.article_revision.submission.person.email,
+    mail(from: @submission.person.email,
  to: @submission.person.email, subject: 'Informacja o nowym zgłoszeniu oraz rejesracji w systemie.')
   end
  end
