592283e3ff963a020b8c99aa854a9b6f1e88d875
Ania <ania.szczepanczyk@gmail.com>
2016-02-20 23:50:46 +0100

    154_setnapoprawka

--- spec/features/mailers/review_mailer_spec.rb
+++ spec/features/mailers/review_mailer_spec.rb
@@ -7,11 +7,11 @@ feature "Zapytanie" do
     include_context "admin login"
 
     background do
-      Person.create!(name: "Joanna", surmame: "Gąska", email: "sz4n14@gmail.com", sex: "kobieta", roles: ['redaktor','recenzent'], discipline: ["psychologia"])
+      Person.create!(name: "Joanna", surname: "Gąska", email: "sz4n14@gmail.com", sex: "kobieta", roles: ['redaktor','recenzent'], discipline: ["psychologia"])
       Issue.create!(volume: 20, year: 2002)
-      Submission.create!(status: "przyjęty", language: "polski", issue: Issue.first, person: Person.first, received: "19-01-2015", polish_title: "Alicja w krainie czarów", english_title: "Alice in Wonderland", english_abstaract: "Little about that story", english_keywords: "Alice")
+      Submission.create!(status: "przyjęty", language: "polski", issue: Issue.first, person: Person.first, received: "19-01-2015", polish_title: "Alicja w krainie czarów", english_title: "Alice in Wonderland", english_abstract: "Little about that story", english_keywords: "Alice")
       ArticleRevision.create!(version: "1.0", received: "21-01-2016", pages: "10", submission: Submission.first)
-      Review.create!(Article_revision: ArticleRevision.first, person: Person.first, status: "wysłane zapytanie", asked: '18-02-2016', deadline: '03-04-2016')
+      Review.create!(article_revision: ArticleRevision.first, person: Person.first, status: "wysłane zapytanie", asked: '18-02-2016', deadline: '03-04-2016')
     end
 
     scenario "zapytanie o sporządzenie recenzji" do
