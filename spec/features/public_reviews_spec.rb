757fff655a63356d36e8ecaebac20a4f08362ea9
Anna Bogusz <anbo222@gmail.com>
2016-02-20 23:23:46 +0100

    Błąd w teście

--- spec/features/public_reviews_spec.rb
+++ spec/features/public_reviews_spec.rb
@@ -2,15 +2,25 @@ require 'rails_helper'
 
 
 feature "Formularz dodania proponowanych recenzentów" do
-   
+  before do
+    person_1 = Person.create!(name:"Andrzej", surname:"Kapusta", sex: "mężczyzna",
+ email:"a.kapusta@gmail.com", roles: ['redaktor'])
+    submission_1 = Submission.create!(language: "polski", received: "18-01-2016",
+ status: "nadesłany", person: person_1, polish_title: "Dlaczego solipsyzm?",
+ english_title: "title1", english_abstract:"abstract1", english_keywords: "tag1, tag2")
+    article_revision_1 = ArticleRevision.create!(version:"1.0", received:"18-01-2016",
+ pages:"5", submission: submission_1)
+  end
+ 
   scenario "Dodawanie proponowanego recenzenta" do
       visit '/public_reviews/new_reviewer'
 
       within("#new_person") do
-        fill_in "Imię", with: "Andrzej"
-        fill_in "Nazwisko", with: "Kapusta"
-        fill_in "E-mail", with: "a.kapusta@gmail.com"
-        select "mężczyzna", from: "Płeć", visible: false
+        select "Dlaczego solipsyzm?", from: "article_revision"
+        fill_in "Imię", with: "Anna"
+        fill_in "Nazwisko", with: "Genialna"
+        fill_in "E-mail", with: "a.genialna@gmail.com"
+        select "kobieta", from: "Płeć", visible: false
       end
       click_button 'Dodaj propozycję'
 
