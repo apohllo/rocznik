f1227611df6f5d2ed024429e1e54f7f5968285d1
gadi-promienna <gadi@gadi.org.pl>
2016-02-21 09:36:09 +0100

    Moze uda mi sie dodac z powrotem niepotrzebnie usuniete wazne pliki. Kto wie?

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -57,41 +57,18 @@ feature "zarządzanie osobami" do
       click_button 'Utwórz'
 
       expect(page).to have_css(".has-error")
-    
-  
-    context "z jedną osobą w bazie danych" do
-     
-      include_context "admin login"
-      
-      before do
+    end
 
+    context "z jedną osobą w bazie danych" do
+      before do
         Person.create!(name: "Andrzej", surname: "Kapusta", email: "a.kapusta@gmail.com",
                       sex: "mężczyzna")
       end
 
-
       scenario "wyświetlenie szczegółów osoby" do
         visit "/people"
         click_link("Kapusta")
         expect(page).to have_css("h3", text: "Andrzej Kapusta")
-
-    end
-
-   # context "z dwoma osobami, które napisały recenzje w bazie danych" do
-     # before do
-    	#person1 =   Person.create!(name: "Izabella", surname: "Kapusta", email: "a.kapusta@gmail.com", discipline: "filozofia", sex:"K", role_inclusion: "R")
-      #  person2=Person.create!(name: "Andrzej", surname: "Kapusta", email: "a.kapusta@gmail.com", discipline: "filozofia", sex: "M", role_inclusion: "R" ) 
-  	#	Review.create!(status: :asked, person_id: person1.person_id, asked: Time.now )
-  
-
-      #end
-
-      scenario "wyświetlenie szczegółów osoby" do
-        visit "/people"
-        click_link("Kapusta")
-
-        expect(page).to have_css("h3", text: "Andrzej Kapusta")
-
         expect(page).to have_css("dd", text: "mężczyzna")
       end
 
@@ -104,7 +81,6 @@ feature "zarządzanie osobami" do
         click_button 'Zapisz'
 
         expect(page).to have_css("img[src*='man.png']")
-
       end
     end
 
@@ -115,7 +91,6 @@ feature "zarządzanie osobami" do
         Person.create!(name: "Wanda", surname: "Kalafior", email: "w.kalafior@gmail.com",
                        competence: "percepcja dźwięki", sex: "kobieta", roles: ["autor", "redaktor"], discipline: ["etyka"])
       end
-    
 
       scenario "wyszukanie osoby" do
         visit "/people"
@@ -125,7 +100,6 @@ feature "zarządzanie osobami" do
         expect(page).to have_content("Wanda")
         expect(page).not_to have_content("Andrzej")
       end
-
 
       scenario "filtrowanie osob po roli" do
         visit "/people"
@@ -163,7 +137,7 @@ feature "zarządzanie osobami" do
         expect(page).to have_content("Wanda")
         expect(page).to have_content("Andrzej")
       end
-
+      
       scenario "przy usuwaniu zgłoszenia powinno być pytanie, czy użytkownik chce usunąć dane zgłoszenie" do
         visit "/people"
         click_on 'Kalafior'
@@ -212,48 +186,6 @@ feature "zarządzanie osobami" do
         click_on 'Zapisz'
         expect(page).to have_content("Recenzuje po terminie")
       end
->>>>>>> bdfe745a93b96622d2337098027f162f9de90a7d
     end
-
-
-    context "Z uzytkownikiem, ktory ma pięć recenzji" do
-      include_context "admin login"
-      before do 
-        person_1 = Person.create!(name: "Andrzej", surname: "Ziemniak", email: "a.ziemniak@gmail.com", discipline: "filozofia", competence: "percepcja wzrokowa", sex: "mężczyzna", roles: ["autor"])
-        person_2 = Person.create!(name: "Andrzej", surname: "Marchew", email: "a.marchew@gmail.com", discipline: "filozofia", competence: "percepcja dźwięki", sex: "mężczyzna", roles: ["recenzent"])
-        person_3 = Person.create!(name: "Agata", surname: "Kalarepa", email: "a.kalarepa@gmail.com", discipline: "filozofia", competence: "percepcja dźwięki", sex: "kobieta", roles: ["recenzent"])
-        submission = Submission.create!(language: "polski", received: "18-01-2016", status: "nadesłany", person: person_1, polish_title: "Arystoteles.", english_title: "title2", english_abstract: "abstract2",english_keywords: "tag1, tag2")
-        
-        5.times{|i|
-          article_revision[i] = ArticleRevision.create!(version:"#{i}.0", received:"#{i+1}-01-2016", pages:"5", submission: submission)
-        }
-        
-        5.times{|j|
-          Review.create!(status: "recenzja pozytywna", content: " ", asked: "#{j+1}-02-2016", deadline: "16-01-2017", person:
-                       person_2, article_revision: article_revision[j])
-        }
-          
-          end
-
-      scenario "wyświetlenie szczegółów osoby" do
-        visit "/people"
-        click_link("Marchew")
-        expect(page).to have_content("Gratulujemy i bardzo dziękujemy!")
-        expect(page).to have_content("5")
-        end
-
-      scenario "wyświetlenie szczegółów osoby" do
-        visit "/people"
-        click_link("Kalarepa")
-        expect(page).not_to have_content("Gratulujemy i bardzo dziękujemy!")
-        
-        end
-
-      end
-
-
-end
-
   end
 end
-
