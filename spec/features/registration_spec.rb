1373f17b939a137433731919b4cf5932b7b95e44
Barbara Masiulaniec <barbaramasiulaniec@host.com>
2016-02-23 00:27:15 +0100

    testy d

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,21 +6,24 @@ feature "Nowy formularz rejestracji pozwalający na podanie danych osobowych" do
     visit '/users/sign_up'
 
     within("#new_user") do
-      fill_in "email", with: "andrzej.kapusta@gmail.com"
+      fill_in "Adres e-mail", with: "andrzej.kapusta@gmail.com"
+      fill_in "Hasło", with: "asdfasdf"
+      fill_in "Potwierdzenie hasła", with: "asdfasdf"
     end
     click_button 'Zarejestruj się'
+
+      expect(page).to have_content 'Podaj dane'
   end
+ 
 
   scenario "Dodanie danych osobowych" do
     visit '/users/new_person'
 
     within("#new_person") do
-      fill_in "imię", with: "Andrzej"
-      fill_in "nazwisko", with: "Kapusta"
-	  select "mężczyzna", from: "płeć", visible: false
+      fill_in "Imię", with: "Andrzej"
+      fill_in "Nazwisko", with: "Kapusta"
+	  select "mężczyzna", from: "Płeć", visible: false
     end
     click_button 'Zarejestruj się'
-
-    expect(page).to have_css(".has-error")
   end
 end
