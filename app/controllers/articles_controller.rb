0b3fc442387b9b79f352b0b4d99a2556ec9da0d8
korowadlo <andrzej.maleszewski@uj.edu.pl>
2016-02-22 18:51:13 +0000

    przeniesienie ze starej galezi. nowy master. automatyczny przydzial issue_position podczas publikacji numeru. reczny przydzial wolnych pozycji w edycji artykulu, dynamiczne ustalanie hierarchii w widoku numeru rocznika.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,16 +19,27 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update_attributes(article_params)
-      redirect_to @article
+    on_success = false
+
+    if request.format == :json
+      @issue = Issue.where("volume = ?", params[:issue_volume]).find_by(year: params[:issue_year])
+      @article = Article.find_by(submission_id: params[:submission_id])
+      if @article.update_article_position(params[:article_position].eql?("article-up") ? "article-up" : "")
+        on_success = true
+      end
     else
-      render :edit
+      @article = Article.find(params[:id])
+      if @article.update_attributes(article_params)
+        return redirect_to @article
+      else
+        return render :edit
+      end
     end
+    render :json => { ok: on_success }
   end
 
   private
   def article_params
-    params.require(:article).permit(:issue_id, :status, :pages, :external_link, :DOI)
+    params.require(:article).permit(:issue_id, :status, :pages, :external_link, :DOI, :issue_position)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,8 @@ class Article < ActiveRecord::Base
     "opublikowany" => :published
   }
   validates :status, presence: true, inclusion: STATUS_MAPPING.keys
+  validates :issue_position, on: :patch, numericality: { only_integer: true, greater_than_or_equal: 1}
+  after_create :assign_issue_position
   belongs_to :issue
   belongs_to :submission
 
@@ -91,5 +93,59 @@ class Article < ActiveRecord::Base
 
   def to_param
     [id, title.parameterize].join("-")
+  end
+
+  def map_position
+    articles = get_articles
+    table = Array.new
+    articles.each do |article|
+      table << article.issue_position
+    end
+    table
+  end
+
+  def update_article_position down = ""
+    articles = get_positions
+    if down.empty?
+      article_position_flip [articles[:article_next]]
+    else
+      article_position_flip [articles[:article_prev]]
+    end
+  end
+
+  private
+  def get_positions
+    articles = get_articles
+
+    {article_next: get_next_article(articles),
+     article_prev: get_prev_article(articles)}
+  end
+
+  def get_articles
+    Article.all.where("issue_id = ?", self.issue_id)
+  end
+
+  def get_next_article articles
+    articles.where("issue_position > ?", self.issue_position).order(issue_position: :asc).limit(1).first
+  end
+
+  def get_prev_article articles
+    articles.where("issue_position < ?", self.issue_position).order(issue_position: :desc).limit(1).first
+  end
+
+  def article_position_flip article
+    if !article[0].nil?
+      halp = self.issue_position
+      self.update_attributes(issue_position: article[0].issue_position)
+      article[0].update_attributes(issue_position: halp)
+    end
+  end
+
+  def assign_issue_position
+    if !get_articles.nil?
+      taken_position = map_position
+      taken_position = (1..taken_position.length+5).to_a - taken_position
+      self.update_column(:issue_position, taken_position[0])
+    end
   end
 end
