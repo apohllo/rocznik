dbff8b204aed698fe14df5d8b79426c51c75eeb6
Ania <ania.szczepanczyk@gmail.com>
2016-02-23 12:50:18 +0100

    155_testy2_błąd

--- spec/features/mailers/review_mailer_spec.rb
+++ spec/features/mailers/review_mailer_spec.rb
@@ -21,15 +21,13 @@ feature "Zapytanie" do
       click_link 'Alicja w krainie czarów'
       click_link 'Wyślij zapytanie o sporządzenie recenzji'
       open_email('sz4n14@gmail.com')
+      current_email.save_and_open
       expect(current_email).to have_content 'Gąska'
       expect(current_email).to have_content 'Alicja w krainie czarów'
       expect(current_email).to have_content 'Little about that story'
-    end
-
-    scenario "akceptacja recenzji" do
-      open_email('sz4n14@gmail.com')
-      current_email.save_and_open
-      expect(current_email).to have_current_path(accepted_review_url(@review))
+      expect(current_email).to have_link 'Akceptuj recenzję'
+      click_on 'Akceptuj recenzję'
+      expect(page).to have_content 'recenzja przyjęta'
     end
   end
 end
