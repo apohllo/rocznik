55bab4d65b90fcbf990066c0ecefb5ec98ecbdda
roksana1 <roksanawantulok@gmail.com>
2016-02-23 14:30:45 +0000

    Porządki w mejlach

--- app/mailers/newperson_mailer.rb
+++
@@ -1,7 +0,0 @@
-class NewuserMailer < ApplicationMailer
-  def add(person)
-    @person = person
-    mail(from: @submission.person.email,
- to: @submission.person.email, subject: 'Informacja o nowym zgłoszeniu oraz rejesracji w systemie.')
-  end
- end
