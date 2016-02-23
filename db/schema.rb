b08a4c5de47c6c30b3e24cb2bd61c1468e217fa8
Magdalena Goryl <magdagoryl@gmail.com>
2016-02-23 18:32:21 +0100

    konflikt 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217185248) do
+ActiveRecord::Schema.define(version: 20160223153523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -336,8 +336,8 @@ ActiveRecord::Schema.define(version: 20160217185248) do
     t.text     "funding"
     t.date     "received"
     t.string   "language"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "person_id"
     t.integer  "issue_id"
   end
