6fb98619d68d1fa8545ab4ead5f4c966f2071f47
clodia <grzegoleck@gmail.com>
2016-02-26 13:34:25 +0000

    poprawione

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -4,11 +4,11 @@ class UserSubmissionsController < ApplicationController
   before_action :user_required  
 
   def index
-    @submissions = current_person.submissions
+    @submissions = current_person.authorships.map(&:submission)
   end
 
   def show
-    @submission = current_person.submissions.find(params[:id])
+    @submission = current_person.authorships.map(&:submission)
     if @submission.nil?
       redirect_to user_submissions_path
     end
