80e48b30fc8419ce308fb1a7a3805acdedd3d56a
JoannaBy <joanna.byszuk@gmail.com>
2016-03-01 23:39:32 +0100

    46-formularz_zgloszenia_nowej_wersji

--- spec/features/public_revisions_spec.rb
+++ spec/features/public_revisions_spec.rb
@@ -7,14 +7,16 @@ feature "publiczne dodawanie nowej wersji" do
         Person.create!(name: "Andrzej", surname: "Kapusta", email: "a.kapusa@gmail.com", sex:
                    "mężczyzna", roles: ['autor'])
         Submission.create!(person_id: Person.first, status: "nadesłany", polish_title: "Ulisses",
-                           received: '29-02-2016', english_title: "Ulisses", english_abstract: "A tale of a man", english_keywords: "ulisses", language: "polski", issue: Issue.first)
+                           received: '29-02-2016', english_title: "Ulisses", english_abstract: "A tale of a man", english_keywords: "ulisses", language: "polski")
       end
 
       scenario "Dodawanie nowej wersji" do
         visit '/submissions/'
         click_on("Ulisses")
+
         click_on("Dodaj wersję")
 
+ 	fill_in "Otrzymano", with: "16/07/2016"
         fill_in "Liczba stron", with: '3'
         fill_in "Liczba ilustracji", with: '5'
         attach_file("Artykuł", 'spec/features/files/plik.pdf')
@@ -22,23 +24,28 @@ feature "publiczne dodawanie nowej wersji" do
 
         within("#version") do
           expect(page).to have_content("plik.pdf")
+	  expect(page).to have_content("1-03-2016")
           expect(page).to have_css("a[title='Edytuj komentarz']")
         end
       end
 
       scenario "Dodawanie nowej wersji bez pliku" do
         visit '/submissions/'
+
         click_on("Ulisses")
         click_on("Dodaj wersję")
 
+	fill_in "Otrzymano", with: "16/07/2016"
         fill_in "Liczba stron", with: '3'
         fill_in "Liczba ilustracji", with: '5'
         click_button 'Dodaj'
 
         within("#new_article_revision") do
+	  expect(page).to have_content("1-03-2016")
           expect(page).to have_css('.has-error')
         end
       end
+
     end
 end
 
