55bbfe2490ff478178d7daf4c224657692e43607
gadi-promienna <gadi@gadi.org.pl>
2016-03-03 23:27:07 +0100

    Migracje poprawiajace bledy w schemacie tabeli message. Zapisywanie wiadomosci. Jeszcze poprawiam  bledy. Potem zostaną testy i widok  wiadomosci w controllerze.

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -1,13 +1,27 @@
 class SubmissionMailer < ApplicationMailer
 
-  def send_contract(person)
-    @person = person
+  def send_contract(submission)
+    @submission = submission
+    @person = @submission.person
+    params = {:addresse => @person.email, 
+    		  :subject => 'Umowa wydawnicza', 
+    		  :submission=> @submission.id}
     attachments["Umowa-wydawnicza.pdf"] = File.read("#{Rails.root}/public/plik.pdf")
-    mail(to: @person.email, subject: 'Umowa wydawnicza')
+    mail(to: @params[:addresse], subject: @params[:subject])
+    @message = Message.new(addresse: @params[:addresse], subject: @params[:subject], 
+    submission: @params[:submission])
+    @message.save()
   end
 
   def send_decision(submission)
     @submission = submission
-    mail(to: @submission.person.email, subject: 'Decyzja - Rocznik Kognitywistyczny')
+    @params = {:addresse => @submission.person.email, 
+    		  :subject => 'Decyzja - Rocznik Kognitywistyczny', 
+    		  :submission=> @submission.id}
+    mail(to: @params[:addresse], subject: @params[:subject])
+    @message = Message.new(addresse: @params[:addresse], subject: @params[:subject], 
+    submission: @params[:submission])
+    @message.save()
+     
   end
 end
