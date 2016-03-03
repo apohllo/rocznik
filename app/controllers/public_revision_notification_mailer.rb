50a8a9fc8a634f8ba1a6d959d149e2fec96bfc8d
JoannaBy <joanna.byszuk@gmail.com>
2016-03-03 13:18:12 +0100

    48-mail_o_nowej_wersji

--- app/controllers/public_revision_notification_mailer.rb
+++
@@ -1,6 +0,0 @@
-class PublicRevisionMailer < ApplicationMailer
-  def public_revision_notification(revision)
-    @revision = article_revision
-    mail(from: @revision.person.email, to: @revision.person.email, subject: 'Nowa wersja artykułu')
-  end
-end
