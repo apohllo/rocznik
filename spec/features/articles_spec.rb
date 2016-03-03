991dd87ae4c17635ddb194120967ee482949a489
Ola Czechonska <o.czechonska@gmail.com>
2016-03-03 17:40:05 +0100

    Próbne podejście do testu

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -119,7 +119,7 @@ feature "Artykuły" do
         expect(page).to have_content("Wiemy wszystko")
         expect(page).not_to have_content("Jerzozwież")
       end
-      
+
       scenario "filtrowanie artykułów po tytule" do
         visit "/articles"
         fill_in "Tytuł", with: "Wiemy wszystko"
@@ -138,18 +138,26 @@ feature "Artykuły" do
         click_on("Tytuł")
         expect(page).to have_content(/Wiemy wszystko.*Jerzozwież/)
       end
-      
+
       scenario "filtrowanie artykułów po redaktorze" do
         visit "/articles"
-        
+
         select "Adam Kapusta", from: "Redaktor"
-        
+
         click_on("Filtruj")
         expect(page).to have_content(/Wiemy wszystko.*Jerzozwież/)
-        
+
         click_on("Filtruj")
         expect(page).not_to have_content(/Jerzozwież.*Wiemy wszystko/)
       end
+
+      scenario "Licznik wyświetleń artykułu" do
+        visit "/articles"
+
+        click_on 'Wiemy wszystko'
+        expect(page).to have_content("Liczba wyświetleń artykułu: 1")
+      end
+      
     end
   end
 end
